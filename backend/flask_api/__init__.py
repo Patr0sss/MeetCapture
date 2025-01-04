2ddfb89f816836eb5e0d9a7ee2865bea42b0ac14
JaSycz <kubasy2019@gmail.com>
2025-01-04 20:42:27 +0100

    CORS

--- backend/flask_api/__init__.py
+++ backend/flask_api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from pymongo import MongoClient
+from flask_cors import CORS
 import json
 from flask_jwt_extended import JWTManager
 
@@ -8,6 +9,15 @@ def create_app():
     global db
 
     app = Flask(__name__)
+
+    CORS(app, resources={
+    r"/*": {
+        "origins": ["chrome-extension://*"], 
+        "methods": ["GET", "POST", "PUT", "DELETE"],
+        "allow_headers": ["Content-Type", "Authorization"],
+        "supports_credentials": True
+    }
+    })
     configure_app(app)
     jwt.init_app(app)
     client = MongoClient(app.config["DB_URI"])
