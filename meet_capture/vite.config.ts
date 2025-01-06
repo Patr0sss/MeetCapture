9aaf12ddae0d788885284d90690cae0b0859cad1
niezgodam <162017567+poteznymichu@users.noreply.github.com>
2025-01-06 15:17:45 +0100

    config updated allows imports

--- meet_capture/vite.config.ts
+++ meet_capture/vite.config.ts
@@ -1,22 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
-        background: "src/background.ts",
-        contentScript: "src/contentScript.ts",
-        offscreenScript: "src/offscreenScript.ts",
-        offscreen: "offscreen.html",
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "src/background.ts"),
+        contentScript: resolve(__dirname, "src/contentScript.ts"),
+        offscreenScript: resolve(__dirname, "src/offscreenScript.ts"),
+        offscreen: resolve(__dirname, "offscreen.html"),
       },
       output: {
         entryFileNames: "[name].js",
         chunkFileNames: "[name].js",
         assetFileNames: "[name].[ext]",
       },
+    },
+    outDir: "dist", 
+    target: "esnext", 
+    sourcemap: false, 
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), 
     },
   },
 });
